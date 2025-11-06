138049567a011f101b1b732055d334d0e9d02009
Borbiró Dávid <dborbiro@gmail.com>
2025-11-06 13:11:59 +0100

    Első commit

---
+++ FR2_project_5/main.c
@@ -0,0 +1,18 @@
+/*
+ * FR2_project_5.c
+ *
+ * Created: 2025. 11. 06. 12:53:22
+ * Author : dborb
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
